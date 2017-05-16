6ff9e140e3c386ae473f2ee49174ae6bfbf824dc
Ivan Kaho Chan <ivan.kaho.chan@gmail.com>
2017-05-16 14:27:13 -0400

    Has margins between text

--- index.js
+++ index.js
@@ -14,12 +14,18 @@ const App = {
 
         const li = document.createElement('li')
         li.textContent = form.personName.value
+        //li.style.marginRight = '5px'
+
         const delbutton = document.createElement('button')
         const probutton = document.createElement('button')
         delbutton.innerHTML = "Delete"
         probutton.innerHTML = "Promote"
+        delbutton.style.marginLeft = '5px'
+        probutton.style.marginLeft = '5px'
+
         li.appendChild(delbutton)
         li.appendChild(probutton)
+
         App.list.insertBefore(li, App.list.firstChild)
         details.appendChild(App.list)
         delbutton.addEventListener('click', this.handleDelete)
@@ -40,7 +46,6 @@ const App = {
         //console.log(hi)
         const form = event.target
         node = form.parentNode
-
         node.style.border = "thin dotted red"
     },
 }
